1926080145d449b23458a5f6d713a2bbaf2ea036
bobpower <bobpower42@gmail.com>
2016-02-19 17:09:40 +0000

    remove processing

--- halfnes/src/com/grapeshot/halfnsf/halfNSF.java
+++ halfnes/src/com/grapeshot/halfnsf/halfNSF.java
@@ -2,47 +2,19 @@ package com.grapeshot.halfnsf;
 
 import java.io.File;
 
-import processing.core.PApplet;
-
-
-
-
-
-public class halfNSF  extends PApplet {
+public class halfNSF {
 
     
     public static NES nes;
     public static void main(String[] args) {
-        String[] a = {"MAIN"};
-        PApplet.runSketch( a, new halfNSF());
+    	nes=new NES();
     }
-    public void settings(){
-        size(200,200,P2D); 
-        smooth();
-        
-    }
-    public void setup(){
-    	nes=new NES();    
-    }
-    public void draw() {
-        background(128); 
-    }
-    public void mousePressed(){
-    	selectInput("Select a file to process:", "fileSelected");    
-    }
-    public void fileSelected(File selection) {
-    	  if (selection == null) {
-    	    println("Window was closed or the user hit cancel.");
-    	  } else {
-    	    //nes.loadROM(selection.getAbsolutePath());
-    		  println("load");
-    	    nes.loadROM(selection.getAbsolutePath());
-    	    println(nes.getrominfo());
+    
+    public void LoadNSF(File selection) {
+    	  if (selection != null) {
+    	  nes.loadROM(selection.getAbsolutePath());
     	    Thread t=new Thread(nes);
     	    t.start();    	  }
-    	}
-    
-    public void startNSF(){
-    	nes.run();    
-    }
+    	} 
+
 }
